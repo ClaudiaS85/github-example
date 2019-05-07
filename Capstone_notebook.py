ebfcdb46ea263a2f0ace4f7572262e14753eb4e4
Claudia Sacco <sacco@bmtecnologie.net>
2019-05-07 17:13:21 +0200

    Notebook for Capstone

---
+++ Capstone_notebook.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Hello! _This notebook will be mainly used for the capstone project_
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
